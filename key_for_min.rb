fca92b3ff0a71dd4e87a7512a6c6625cd054ff43
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-06 08:10:41 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,7 @@
 def key_for_min_value(name_hash)
   smallest = nil
   name_hash.each do |name, value|
-    if smallest == nil
-      smallest = name
-    elsif name[value] < smallest[value]
+    if smallest == nil || value < name_hash[smallest]
       smallest = name
     end
   end
